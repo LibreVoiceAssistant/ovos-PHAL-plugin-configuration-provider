74f64f913bace298765b62713afa258e7e3e4c73
Aditya Mehra <aix.m@outlook.com>
2022-07-19 18:22:49 +0930

    Refactor initial configuration generation and settings

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+import os
 from setuptools import setup
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
 
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-configuration-provider=ovos_PHAL_plugin_configuration_provider:ConfigurationProviderPlugin'
 setup(
@@ -12,8 +19,10 @@ setup(
     author_email='aix.m@outlook.com',
     license='Apache-2.0',
     packages=['ovos_PHAL_plugin_configuration_provider'],
+    package_data={'': package_files('ovos_PHAL_plugin_configuration_provider')},
     install_requires=["ovos-plugin-manager>=0.0.1"],
     zip_safe=True,
+    include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
